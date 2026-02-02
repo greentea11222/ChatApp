228d8b81f341e898ebf17acb16c0c861e173d154
greentea11222 <oregreen1122@gmail.com>
2026-02-03 00:54:58 +0900

    Serviceを編集

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,22 @@
 package com.example.demo.service;
 
-//ユーザー登録時のパスワード暗号化などのビジネスロジック
-public class UserService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
+import com.example.demo.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+//ユーザー登録時のパスワード暗号化などのビジネスロジック
+@Service
+@RequiredArgsConstructor //private finalなフィールドのコンストラクタを自動生成する
+public class UserService implements UserDetailsService {
+	
+	private final UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String name) {
+		return ???;
+	}
 }
