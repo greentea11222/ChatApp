6cea2918d8826018662a29578a7ce4f859cef708
greentea11222 <oregreen1122@gmail.com>
2026-02-04 00:53:21 +0900

    UserServiceのメソッドを追加

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.repository.UserRepository;
@@ -15,8 +16,10 @@ public class UserService implements UserDetailsService {
 	
 	private final UserRepository userRepository;
 	
+	//ユーザー名をDBで探し、見つかったらUserインスタンスを返す
 	@Override
 	public UserDetails loadUserByUsername(String name) {
-		return ???;
+		return userRepository.findByName(name)
+				.orElseThrow(() -> new UsernameNotFoundException("ユーザーが見つかりません" + name));
 	}
 }
