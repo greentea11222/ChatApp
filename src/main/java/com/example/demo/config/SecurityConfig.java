983dcb76172ad3efbe610c1da62e981241c79bc3
greentea11222 <oregreen1122@gmail.com>
2026-02-04 23:24:07 +0900

    SecurityConfigの内容を記載中

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,25 @@
 package com.example.demo.config;
 
-//ログイン設定やアクセス制限の設定
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+//ログイン設定やアクセス制限の設定
+@Configuration		//設定ファイルであることを示す
+@EnableWebSecurity	//Spring Security機能を有効にする
+public class SecurityConfig {
+	//パスワードを暗号化（ハッシュ化）する
+	@Bean	//このメソッドが返すオブジェクトをSpringの管理下において使い回せるようにする
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+	}
+	
 }
