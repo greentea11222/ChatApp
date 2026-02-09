219f28587ed37535b87c31d260cf5635b9c65f8b
greentea11222 <oregreen1122@gmail.com>
2026-02-09 21:20:34 +0900

    LoginControllerとUserServiceの修正（パスワード暗号化をServiceで行うようにする）

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -5,10 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 //ログイン・会員登録画面の制御
 @Controller
+@RequiredArgsConstructor //フィールドを引数として使うコンストラクタを自動で作ってくれる
 public class LoginController {
+	
+	//クラスに@RequiredArgsConstructorアノテーションをつけるか、手動でコンストラクタを作らないと
+	//「初期化されていない可能性がある」と言われてエラーになる
+	private final UserService userService;
 	
 	//ログイン画面を表示
 	@GetMapping("/login")
@@ -26,8 +34,8 @@ public class LoginController {
 	//会員登録画面から送られてきたデータを保存する
 	@PostMapping("/register")
 	public String register(User user) {
-		//パスワードを暗号化して保存
-		
+		//サービスに処理を任せる
+		userService.registerUser(user);
 		
 		return "redirect:/login"; //登録が終わったらログイン画面に遷移
 	}
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,8 +3,11 @@ package com.example.demo.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.Role;
+import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +18,22 @@ import lombok.RequiredArgsConstructor;
 public class UserService implements UserDetailsService {
 	
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	
+	//会員登録のメソッド
+		public void registerUser(User user) {
+			//メモ：ここにユーザー名が重複しないかチェックを入れる
+			
+			//パスワードを暗号化
+			String encodedPassword = passwordEncoder.encode(user.getPassword());
+			user.setPassword(encodedPassword);
+			
+			//権限を設定（一律でUSER権限に）
+			user.setRole(Role.ROLE_USER);
+			
+			//DBに保存
+			userRepository.save(user);
+		}
 	
 	//ユーザー名をDBで探し、見つかったらUserインスタンスを返す
 	@Override
